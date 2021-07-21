404bd7b58f57eb0b7be766fb41f6a37a3c96a475
beast001 <39857500+beast001@users.noreply.github.com>
2021-07-21 16:02:45 +0300

    added postgres database configuration

--- config/database.php
+++ config/database.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Support\Str;
+$DATABASE_URL=parse_url('//rciqwvxgpbulmq:37987e0fba7a9ff387100566865901ec12ec7bfc7bc5ea6aa988525f0a6395fb@ec2-52-6-211-59.compute-1.amazonaws.com:5432/daglccane0sjov');
 
 return [
+
 
     /*
     |--------------------------------------------------------------------------
@@ -15,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,17 +67,15 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' =>$DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
