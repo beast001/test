e2086ffc18eb7f9ca40be84189ac0c7c3820974a
beast001 <39857500+beast001@users.noreply.github.com>
2021-07-22 13:42:40 +0300

    Procfile update

--- config/database.php
+++ config/database.php
@@ -1,10 +1,8 @@
 <?php
 
 use Illuminate\Support\Str;
-$DATABASE_URL=parse_url('//rciqwvxgpbulmq:37987e0fba7a9ff387100566865901ec12ec7bfc7bc5ea6aa988525f0a6395fb@ec2-52-6-211-59.compute-1.amazonaws.com:5432/daglccane0sjov');
 
 return [
-
 
     /*
     |--------------------------------------------------------------------------
@@ -17,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -67,15 +65,17 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => $DATABASE_URL["host"],
-            'port' =>$DATABASE_URL["port"],
-            'database' => ltrim($DATABASE_URL["path"], "/"),
-            'username' => $DATABASE_URL["user"],
-            'password' => $DATABASE_URL["pass"],
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
+            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'require',
+            'sslmode' => 'prefer',
         ],
 
         'sqlsrv' => [
